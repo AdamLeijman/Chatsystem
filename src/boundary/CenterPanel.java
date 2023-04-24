cacbe050ee373380a9d9ec9e53f44307fbb1bc6c
martin24olsson <128680764+martin24olsson@users.noreply.github.com>
2023-04-24 09:34:41 +0200

    Update CenterPanel.java

--- src/boundary/CenterPanel.java
+++ src/boundary/CenterPanel.java
@@ -39,8 +39,7 @@ public class CenterPanel <T> extends JPanel implements ActionListener {
     }
 
     public void addToDialog(T[] obj){
-        MyCellRenderer cr = new MyCellRenderer();
-        dialog.setCellRenderer(cr);
+        //dialog.setCellRenderer(cr);
         dialog.setListData(obj);
     }
 
@@ -54,35 +53,6 @@ public class CenterPanel <T> extends JPanel implements ActionListener {
         String buttonText = ((JButton) e.getSource()).getText();
     }
 
-    class MyCellRenderer extends JLabel implements ListCellRenderer<Object> {
-        final static ImageIcon longIcon = new ImageIcon("long.gif");
-        final static ImageIcon shortIcon = new ImageIcon("short.gif");
 
-        // This is the only method defined by ListCellRenderer.
-        // We just reconfigure the JLabel each time we're called.
-
-        public Component getListCellRendererComponent(
-                JList<?> list,           // the list
-                Object value,            // value to display
-                int index,               // cell index
-                boolean isSelected,      // is the cell selected
-                boolean cellHasFocus)    // does the cell have focus
-        {
-            String s = value.toString();
-            setText(s);
-            setIcon((s.length() > 10) ? longIcon : shortIcon);
-            if (isSelected) {
-                setBackground(list.getSelectionBackground());
-                setForeground(list.getSelectionForeground());
-            } else {
-                setBackground(list.getBackground());
-                setForeground(list.getForeground());
-            }
-            setEnabled(list.isEnabled());
-            setFont(list.getFont());
-            setOpaque(true);
-            return this;
-        }
-    }
 
 }
