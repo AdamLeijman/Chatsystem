5bf5695949bdef69797421fe44b19da72067cd5c
martin24olsson <128680764+martin24olsson@users.noreply.github.com>
2023-05-10 15:18:37 +0200

    a

--- src/entity/Clients.java
+++ src/entity/Clients.java
@@ -1,44 +1,22 @@
 package entity;
 
-import controller.User;
+import controller.Client;
 
 import java.io.Serializable;
 import java.util.HashMap;
-import java.util.Map;
 
 public class Clients implements Serializable {
     private HashMap<User, Client> clients = new HashMap<User, Client>();
     // egna tillägg
-    public synchronized void put(User, Client) {
+    public synchronized void put(User user,Client client) {
         clients.put(user,client);
     }
-    public synchronized User get(entity.User user) {
+    public synchronized Client get(User user) {
         return get(user);
     }
 
-    public synchronized void remove(entity.User user, User client){
+    public synchronized void remove(User user, Client client){
         clients.remove(user, client);
-    }
-
-    public synchronized String[] getList(){
-        String[] temp = new String[100];
-        int count = 0;
-        for(Map.Entry<entity.User, User> set : clients.entrySet()){
-            temp[count] = set.getKey().getUsername();
-            count++;
-        }
-        return temp;
-    }
-
-    public synchronized entity.User[] getUsers(){
-        entity.User[] temp = new entity.User[10];
-        int count = 0;
-        for(Map.Entry<entity.User, User> set : clients.entrySet()){
-            temp[count] = set.getKey();
-            System.out.println("----------" + temp[count].getUsername());
-            count++;
-        }
-        return temp;
     }
     // fler synchronized-metoder som behövs
 }
