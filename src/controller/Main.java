edfac9bccf31cbb8f57e0a5352e01cac9245faef
martin24olsson <128680764+martin24olsson@users.noreply.github.com>
2024-02-16 10:41:12 +0100

    comment

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,11 +1,21 @@
 package controller;
 
+import java.util.function.ToDoubleBiFunction;
+
 public class Main {
 
 
     public static void main(String[] args) {
         //OBS! JAG HAR LADDAT UPP ETT DOKUMENT BASERAT PÅ RAPPORTMALLEN
         //https://docs.google.com/document/d/1ult885RAV0reZsTqXJDE6Oa735izRSVT/edit?usp=sharing&ouid=115394361315385912023&rtpof=true&sd=true
+
+        /*To DO list
+        //1. Contaktfönster
+        2. Logga tid på hårddisk
+        2. Refaktorera klasser
+        //2. kommentera text
+        4 lägga till användarnamn + bild vid inlogg
+         */
 
         Server server = new Server(1441);
         server.start();
@@ -19,12 +29,12 @@ public class Main {
         new Client("John", 1);
         new Client("Josephine", 2);
 
-        /*
+
         try {
             Thread.sleep(6000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        new Client("Ewa", 3);*/
+        new Client("Ewa", 3);
     }
 }
